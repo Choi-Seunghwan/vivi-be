151cdc76fa55677e2fdc8508fad2796132dd4c9d
Choi Seunghwan <49161475+Choi-Seunghwan@users.noreply.github.com>
2022-09-19 23:43:23 +0900

    feat: nest middleware document 파악 중

---
+++ src/users/users.middleware.ts
@@ -0,0 +1,9 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+
+@Injectable()
+export class getMeMiddleware implements NestMiddleware {
+  use(req: any, res: any, next: (error?: any) => void) {
+    console.log('@@ getMeMiddleware');
+    next();
+  }
+}
