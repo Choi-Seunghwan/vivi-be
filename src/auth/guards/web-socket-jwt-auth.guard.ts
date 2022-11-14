ed600f49da9c3174c8192b11485f77cf0be9dab2
Choi Seunghwan <choiseunghwan.tech@gmail.com>
2022-11-14 20:42:05 +0900

    ✨ feat: websocket guard getRequest 오버라이잉

--- src/auth/guards/web-socket-jwt-auth.guard.ts
+++ src/auth/guards/web-socket-jwt-auth.guard.ts
@@ -1,5 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class webSocketJwtAuthGuard extends AuthGuard('webSocketJwt') {}
+export class webSocketJwtAuthGuard extends AuthGuard('webSocketJwt') {
+  constructor() {
+    super();
+  }
+
+  getRequest(ctx: ExecutionContext) {
+    console.log('@@ gerRequset');
+
+    return ctx.switchToWs().getClient().handshake;
+  }
+}
