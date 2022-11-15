27ab7ab3108b712686c43825fcfc0105aee8237e
Choi Seunghwan <choiseunghwan.tech@gmail.com>
2022-11-15 21:37:11 +0900

    ✨ feat: cache service 작업

--- src/cache/cache.service.ts
+++ src/cache/cache.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
-
+import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
+import Cache from 'cache-manager';
 @Injectable()
-export class CacheService {}
+export class CacheService {
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+
+  async set(key: string, value: any) {
+    const result = await this.cacheManager.set(key, value);
+    return result;
+  }
+
+  async get(key: string) {
+    const result = await this.cacheManager.get(key);
+    return result;
+  }
+}
